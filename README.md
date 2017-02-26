2ad0e43f9628892a4e16c33404c0c9886caaf164
deepblue_will <deepblue.will@gmail.com>
2017-02-26 22:51:54 +0900

    :+1: First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# SPA note
+This app is sample app that introduced by Web+DB PRESS(Vol.97).
+
+# Require
+- Node.js
+- Yarn
+
+or
+
+- Docker
+
+# Setup
+```sh
+yarn install
+```
+
+or
+
+```
+docker-compose up
+```
+
+# Run
+```
+# Run dev server
+yarn run server
+```
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: {js: './src/main.js'},
+  output: {path: '/public', filename: 'bundle.js'},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './public',
+    port: 8080,
+    inline: true,
+    historyApiFallback: true,
+  },
+}
