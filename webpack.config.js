68c9d0c684ebf8c9dfa7287b5b9ad5f45867d225
deepblue_will <deepblue.will@gmail.com>
2017-03-01 00:35:34 +0900

    :+1: Add Css settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: {js: './src/main.js'},
+  entry: {
+    js: './src/main.js',
+    css: './src/main.css'
+  },
   output: {path: '/public', filename: 'bundle.js'},
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: ['css-loader', 'postcss-loader']
+        }),
+      },
     ],
   },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
   devtool: 'source-map',
   devServer: {
     contentBase: './public',
